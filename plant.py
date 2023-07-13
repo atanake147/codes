ccce1fcc5b71e77ab5a74fc82b5bd3525c7ced07
atanake147 <135542756+atanake147@users.noreply.github.com>
2023-07-13 13:44:17 -0400

    Create plant.py

---
+++ plant.py
@@ -0,0 +1,106 @@
+# import module to write testcases for the code http://www.google.com
+import unittest
+# variables declarations
+transparent = True
+transparency = 2
+translucent = False
+is_cube = False
+glass = False
+
+colliders = []
+# vertices positions
+vertex_positions = [
+	[-0.3536, 0.5000,  0.3536, -0.3536, -0.5000,  0.3536,  0.3536, -0.5000, -0.3536,  0.3536, 0.5000, -0.3536],
+	[-0.3536, 0.5000, -0.3536, -0.3536, -0.5000, -0.3536,  0.3536, -0.5000,  0.3536,  0.3536, 0.5000,  0.3536],
+	[ 0.3536, 0.5000, -0.3536,  0.3536, -0.5000, -0.3536, -0.3536, -0.5000,  0.3536, -0.3536, 0.5000,  0.3536],
+	[ 0.3536, 0.5000,  0.3536,  0.3536, -0.5000,  0.3536, -0.3536, -0.5000, -0.3536, -0.3536, 0.5000, -0.3536],
+]
+# textures coordinates
+tex_coords = [
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+]
+
+# shading values
+shading_values = [
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+]
+# Class Unittest
+class MyCodeTestCase(unittest.TestCase):
+    def setUp(self):
+        # Set up any necessary test data or configurations
+
+        # Initialize the code variables for testing
+        self.transparent = True
+        self.transparency = 2
+        self.is_cube = False
+        self.glass = False
+        self.translucent = False
+        self.colliders = []
+        # Initialize vertices position variables for testing 
+        self.vertex_positions = [
+	[-0.3536, 0.5000,  0.3536, -0.3536, -0.5000,  0.3536,  0.3536, -0.5000, -0.3536,  0.3536, 0.5000, -0.3536],
+	[-0.3536, 0.5000, -0.3536, -0.3536, -0.5000, -0.3536,  0.3536, -0.5000,  0.3536,  0.3536, 0.5000,  0.3536],
+	[ 0.3536, 0.5000, -0.3536,  0.3536, -0.5000, -0.3536, -0.3536, -0.5000,  0.3536, -0.3536, 0.5000,  0.3536],
+	[ 0.3536, 0.5000,  0.3536,  0.3536, -0.5000,  0.3536, -0.3536, -0.5000, -0.3536, -0.3536, 0.5000, -0.3536],
+]
+      
+        # Initialize texture coordinates variables for testing
+        self.tex_coords = [
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+]
+        # Initialize shading values variables for testing
+        self.shading_values = [
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+]
+
+
+    # test transparent variable
+    def test_transparent(self):
+        self.assertTrue(self.transparent)
+    # test transparency variable
+    def test_transparency(self):
+        self.assertEqual(self.transparency, 2)
+    # test is_cube variable
+    def test_is_cube(self):
+        self.assertFalse(self.is_cube)
+    # test glass variable
+    def test_glass(self):
+        self.assertFalse(self.glass)
+    # test translucent variable
+    def test_translucent(self):
+        self.assertFalse(self.translucent)
+    # test colliders variable
+    def test_colliders(self):
+        self.assertIsInstance(self.colliders, list)
+    # test vertex_positions variable
+    def test_vertex_positions(self):
+        for positions in self.vertex_positions:
+            self.assertIsInstance(positions, list)
+            self.assertEqual(len(positions), 12)
+    # test tex_coords
+    def test_tex_coords(self):
+        for coords in self.tex_coords:
+            self.assertIsInstance(coords, list)
+            self.assertEqual(len(coords), 12)
+    # test shading_values
+    def test_shading_values(self):
+        for values in self.shading_values:
+            self.assertIsInstance(values, list)
+            self.assertEqual(len(values), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
